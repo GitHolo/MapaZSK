b341ba66baf39779970ccdbcb4123be55620d05e
GitHolo <ooye2332@gmail.com>
2022-11-08 21:22:08 +0100

    plan lekcji

--- Assets/Scripts/plan.cs
+++ Assets/Scripts/plan.cs
@@ -14,16 +14,29 @@ public class plan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         string hour = System.DateTime.UtcNow.ToLocalTime().ToString("HH");
         string min = System.DateTime.UtcNow.ToLocalTime().ToString("mm");
         int hourInt = int.Parse(hour);
         int minInt = int.Parse(min);
         hourInt = hourInt * 60;
         int time = hourInt + minInt-465;
+        int botTime = 654 - time;
         print(time);
-        RectTransformExtensions.SetTop(rt,time);
-        RectTransformExtensions.SetBottom(rt, time + 6);
+        if(time>0)
+        {
+            RectTransformExtensions.SetTop(rt,time);
+        }
+        else
+        {
+            RectTransformExtensions.SetTop(rt, 0);
+        }
+        if(botTime>0)
+        {
+            RectTransformExtensions.SetBottom(rt, botTime);
+        }
+        else
+        {
+            RectTransformExtensions.SetBottom(rt, 0);
+        }
     }
 }
